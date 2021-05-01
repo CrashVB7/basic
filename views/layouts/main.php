b5e2bf7a83ce46dd97f35250c9a61a6aa2c16b7d
Alexandru Gherasimov <gherasimovalexandru@gmail.com>
2021-05-01 03:54:42 +0300

    Configure Bootstrap 4

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -32,11 +32,11 @@ AppAsset::register($this);
         'brandLabel' => Yii::$app->name,
         'brandUrl' => Yii::$app->homeUrl,
         'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
+            'class' => 'fixed-top navbar-expand-lg navbar-dark bg-dark',
         ],
     ]);
     echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
+        'options' => ['class' => 'navbar-nav ml-auto'],
         'items' => [
             ['label' => 'Home', 'url' => ['/site/index']],
             ['label' => 'About', 'url' => ['/site/about']],
